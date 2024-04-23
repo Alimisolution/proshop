1fb20c88e46b0fdb09076ec09ea93b2d82e0fd93
Alimisolution <alimirosheed5000@gmail.com>
2024-04-23 13:35:34 +0100

    Database connection

--- backend/server.js
+++ backend/server.js
@@ -2,17 +2,20 @@ import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
-dotenv.config();
-import connectDB from './config/db.js';
+import mongoose from 'mongoose';
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
 import uploadRoutes from './routes/uploadRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
+dotenv.config();
 const port = process.env.PORT || 5000;
 
-connectDB();
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log('DATABASE CONNECTED SUCCESSFULLY'))
+  .catch((err) => console.log('ERROR WHILE CONNECTING TO DATABASE'));
 
 const app = express();
 
